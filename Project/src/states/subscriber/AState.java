10de08e600641b33763223c2989af3b6ead433d5
Firas Aboushamalah <fabousha@uwo.ca>
2020-04-26 18:45:25 -0400

    first commit

---
+++ Project/src/states/subscriber/AState.java
@@ -0,0 +1,16 @@
+package states.subscriber;
+
+import events.AbstractEvent;
+
+public class AState implements IState {
+
+	public void handleEvent(AbstractEvent event, String channelName) {
+		
+		//check if the right thing is printed
+		System.out.println("Event "+ event.hashCode() + " in channel " + channelName + " is handled by Astate");	
+	}
+	
+
+}
+
+
---
+++ Project/src/states/subscriber/defaultState.java
@@ -0,0 +1,12 @@
+package states.subscriber;
+
+import events.AbstractEvent;
+
+public class defaultState implements IState{
+
+	public void handleEvent(AbstractEvent event, String channelName) {
+		
+		System.out.println("Event "+ event.hashCode() + " in channel " + channelName + " is handled by defaultstate");
+	}
+
+}
